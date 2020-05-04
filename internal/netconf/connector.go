cc3b1bd0e16e98a16808602536b5a95a8283e708
Roberto D'Auria <roberto@measurementlab.net>
2020-05-04 10:53:27 +0200

    Customize key-exchange algorithms so older JunOS fw versions work.

--- internal/netconf/connector.go
+++ internal/netconf/connector.go
@@ -1,6 +1,14 @@
 package netconf
 
-import "github.com/scottdware/go-junos"
+import (
+	"errors"
+
+	"github.com/Juniper/go-netconf/netconf"
+	"github.com/scottdware/go-junos"
+	"golang.org/x/crypto/ssh"
+)
+
+var newSession = junos.NewSessionWithConfig
 
 // These types provide an abstraction for the underlying connector and
 // connection to a NETCONF-enabled device, so that they can be unit tested.
@@ -16,5 +24,21 @@ type connector interface {
 type junosConnector struct{}
 
 func (junosConnector) NewSession(host string, auth *junos.AuthMethod) (connection, error) {
-	return junos.NewSession(host, auth)
+	var config *ssh.ClientConfig
+
+	if len(auth.PrivateKey) == 0 {
+		return nil, errors.New("no private key specified")
+	}
+	config, err := netconf.SSHConfigPubKeyFile(auth.Username, auth.PrivateKey, auth.Passphrase)
+	if err != nil {
+		return nil, err
+	}
+
+	config.HostKeyCallback = ssh.InsecureIgnoreHostKey()
+
+	// This matches the only two key exchange algorithm we use on our switches.
+	config.Config.KeyExchanges = []string{"curve25519-sha256@libssh.org",
+		"diffie-hellman-group-exchange-sha256"}
+
+	return newSession(host, config)
 }
--- internal/netconf/connector_test.go
+++ internal/netconf/connector_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/scottdware/go-junos"
+	"golang.org/x/crypto/ssh"
 )
 
 func Test_junosConnector_NewSession(t *testing.T) {
@@ -13,4 +14,30 @@ func Test_junosConnector_NewSession(t *testing.T) {
 	if err == nil {
 		t.Errorf("NewSession(): expected err, got nil.")
 	}
+
+	// Attempt to use a non-existing key file.
+	auth := &junos.AuthMethod{
+		PrivateKey: "thiswillfail",
+		Username:   "thiswillfail",
+	}
+
+	_, err = j.NewSession("", auth)
+	if err == nil {
+		t.Errorf("NewSession() expected err, got nil.")
+	}
+
+	// This should succeed.
+	auth.PrivateKey = "testdata/dummy.key"
+	oldNewSession := newSession
+	newSession = func(host string, clientConfig *ssh.ClientConfig) (*junos.Junos, error) {
+		return &junos.Junos{}, nil
+	}
+	s, err := j.NewSession("", auth)
+	if err != nil {
+		t.Errorf("NewSession() expected err, got nil.")
+	}
+	if s == nil {
+		t.Errorf("NewSession() returned nil.")
+	}
+	newSession = oldNewSession
 }
